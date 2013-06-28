af4b0ea051898638b46589df806d245a01c023c1
alienscience <saul@alienscience.org.uk>
2013-06-28 18:50:38 +0200

    Starting with angular.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,9 +2,15 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('alienMail.controllers', []).
+  controller('Vertical', ['$scope', 'Summaries', function($scope, Summaries) {
+      // Get the summaries from the server
+      $scope.summaries = Summaries.query();
 
+      // Function that selects a message when a summary is clicked
+      $scope.selectMessage = function(summary) {
+          $scope.selectedMessage = summary.id;
+      }
   }])
   .controller('MyCtrl2', [function() {
 
